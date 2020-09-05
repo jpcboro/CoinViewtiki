d4914587219fda822051d9d3d85a018bf19ec2fa
jboro <joseph@videomyjob.com>
2020-09-05 14:43:41 +0800

    - Updated Xamarin Forms nuget - Added models

---
+++ CoinViewTiki/Models/Coin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CoinViewTiki.Models
+{
+    public class Coin
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ CoinViewTiki/Models/CoinData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CoinViewTiki.Models
+{
+    public class CoinData : Coin
+    {
+        public Image image { get; set; }
+
+        public Description description { get; set; }
+
+        public MarketData market_data { get; set; }
+    }
+
+    public class MarketData
+    {
+        public CurrentPrice current_price { get; set; }
+    }
+
+    public class Image
+    {
+        public string thumb { get; set; }
+    }
+
+    public class Description
+    {
+        public string en { get; set; }
+    }
+
+    public class CurrentPrice
+    {
+        public double usd { get; set; }
+    }
+}
